4c51bec98ba0a6ffb98031bb279b462d13b36018
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-04 08:26:09 -0300

    refactor: styled components

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,12 +1,22 @@
 import logo from '../../images/logo.svg'
-import './style.css'
+import styled from 'styled-components';
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+  margin-right: 30px;
+`
+
+const Image = styled.img`
+  margin-right: 10px;
+`
 
 function Logo() {
   return (
-    <div className="logo">
-      <img src={logo} alt="logo" className="logo-image" />
+    <LogoContainer>
+      <Image src={logo} alt="logo"/>
       <p><strong>Alura</strong>Books</p>
-    </div>
+    </LogoContainer>
   )
 }
 
